850f12da644643d554af91b04278ac753ab5396f
Prahalad Belavadi <github@belavadi.com>
2019-12-09 13:55:46 +0530

    whitespace

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const PORT = process.env.PORT || 3000;
 const express = require("express");
+
 const app = express();
 app.use(express.json());
+
 const courses = [
     { id: 1, name: "Algorithms" },
     { id: 2, name: "Software Engineering" },
     { id: 3, name: "Human Computer Interaction" }
 ];
+
 app.get("/", function (req, res) {
     //when we get an http get request to the root/homepage
     res.send("Hello World");
 });
+
 //when we route to /courses
 app.get("/courses", function (req, res) {
     res.send(courses); //respond with the array of courses
 });
+
 //To get a specific course, we need to define a parameter id
 app.get("/courses/:id", function (req, res) {
     const course = courses.find(c => c.id === parseInt(req.params.id));
@@ -26,6 +31,7 @@ app.get("/courses/:id", function (req, res) {
     //return the object
     res.send(course);
 });
+
 //using the http post request we can create a new course
 app.post("/courses", function (req, res) {
     //create a course object
@@ -38,6 +44,7 @@ app.post("/courses", function (req, res) {
     //return the course
     res.send(course);
 });
+
 app.put("/courses/:id", function (req, res) {
     //get the course
     const course = courses.find(c => c.id === parseInt(req.params.id));
@@ -50,6 +57,7 @@ app.put("/courses/:id", function (req, res) {
     //return the updated object
     res.send(course);
 });
+
 app.put("/courses/:id", function (req, res) {
     //get the course
     const course = courses.find(c => c.id === parseInt(req.params.id));
@@ -62,6 +70,7 @@ app.put("/courses/:id", function (req, res) {
     //returns the updated object
     res.send(course);
 });
+
 app.listen(PORT, function () {
     console.log(`Listening on Port ${PORT}`);
 });
